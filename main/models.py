26a7aafc10125e57f2a10b1fdf6e639fc6d2046f
lontonggg <reyhan.zada@ui.ac.id>
2023-09-10 19:52:43 +0700

    menambahkan atributes pada models

--- main/models.py
+++ main/models.py
@@ -7,4 +7,5 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    category = models.TextField()
     date_added = models.DateField(auto_now_add=True)
